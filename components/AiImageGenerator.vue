c170a437f47c9f6f0202bdc11f1cfcbb86e41561
Philipp <philipp@geuer-pollmann.de>
2023-11-24 10:02:03 +0100

    feat: added banner

--- components/AiImageGenerator.vue
+++ components/AiImageGenerator.vue
@@ -1,14 +1,17 @@
 <template>
+  <img src="../assets/img/banner.png" alt="Banner">
   <div class="m-5 bg-gray-500  border-gray-800 border-2 rounded-lg w-48 h-48 overflow-hidden">
-    <img :src="imagePath" alt="Icon" class="w-40 h-40 object-contain cursor-pointer items-center"/>
+    <img :src="birdImg" alt="Icon" class="w-40 h-40 object-contain cursor-pointer items-center"/>
   </div>
 </template>
 
 <script>
+import birdImage from '~/assets/img/bird.jpg'
+
 export default {
   data() {
     return {
-      imagePath: "/Users/philipppollmann/Github/SoftwareEngineeringProject/assets/img/Butterfly.jpeg",
+      imagePath: birdImage
     };
   },
   name: 'AiImageGenerator',
