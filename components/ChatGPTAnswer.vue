4532fac8788299ef6d135577eb495931c69c3cc1
Philipp <philipp@geuer-pollmann.de>
2023-12-04 16:02:59 +0100

    feat: image and download button

--- components/ChatGPTAnswer.vue
+++ components/ChatGPTAnswer.vue
@@ -4,8 +4,11 @@
 
 <template>
   <div class="flex items-center justify-center">
-    <div class="bg-gray-500 border-gray-800 border-2 rounded-lg w-5/12 h-52 overflow-hidden p-8">
-      <typing-animation></typing-animation>
+    <div class="bg-gray-500 border-gray-800 border-2 rounded-lg w-5/12 h-1/6 overflow-hidden p-8">
+      <typing-animation></typing-animation> <br/>
+      <h1>Your personal poster:</h1>
+      <img src="~/public/img/anime.jpg" alt="Japan" class="w-52"> <br/>
+      <button class="bg-gradient-to-b w-max mx-auto text-blue-500 font-semibold from-slate-50 to-blue-100 px-10 py-3 rounded-2xl shadow-blue-400 shadow-md border-b-4 hover border-b border-blue-200 hover:shadow-sm transition-all duration-500">Use as Poster</button>
     </div>
   </div>
 
--- components/IdeaHelper.vue
+++ components/IdeaHelper.vue
@@ -7,7 +7,7 @@
     <div style="max-width:700px; margin: 100px auto;">
 
       <form class="flex items-center">
-        <label for="voice-search" class="sr-only">Search</label>
+        <label for="voice-search" class="sr-only">Generate</label>
         <div class="relative w-full">
           <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
             <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
@@ -21,7 +21,6 @@
           <svg aria-hidden="true" class="mr-2 -ml-1 w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>Search
         </button>
       </form>
-
     </div>
   </div>
 
